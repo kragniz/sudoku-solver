63df042593e469783863f01338a5e356a9fed217
Louis Taylor <kragniz@gmail.com>
2013-10-11 22:35:54 +0100

    Add start of assignment

---
+++ src/eu/kragniz/sudoku/Main.java
@@ -0,0 +1,16 @@
+package eu.kragniz.sudoku;
+
+public class Main {
+    /* sextuple-linked list
+                   previous column
+        previous row [cell] next row
+                    next column
+
+                    next cell
+                    previous cell
+     */
+
+    public static void main(String[] args) {
+        System.out.println("hello there");
+    }
+}
---
+++ src/eu/kragniz/sudoku/SudokuParser.java
@@ -0,0 +1,8 @@
+package eu.kragniz.sudoku;
+
+/**
+ * Copyright 2013 Louis Taylor <kragniz@gmail.com>
+ * Licensed under GPLv3 or later
+ */
+public class SudokuParser {
+}
