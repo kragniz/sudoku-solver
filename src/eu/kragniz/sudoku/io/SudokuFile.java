453e338815b5a71af3fd4a6283a0053ffa33481a
Louis Taylor <kragniz@gmail.com>
2013-10-12 23:55:25 +0100

    Basic reading of .sud files

--- src/eu/kragniz/sudoku/io/SudokuFile.java
+++ src/eu/kragniz/sudoku/io/SudokuFile.java
@@ -1,7 +1,7 @@
 package eu.kragniz.sudoku.io;
 
-import java.io.File;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.charset.Charset;
 
 /**
  * Copyright 2013 Louis Taylor <kragniz@gmail.com>
@@ -14,20 +14,33 @@ public class SudokuFile {
 
     public SudokuFile(String filename) {
         this.filename = filename;
+        grid = new int[9][9];
     }
 
     public int[][] getArray() {
         return null;
     }
 
-    public void read() {
+    public void read() throws IOException {
         int x = 0;
         int y = 0;
 
-        File file = new File(filename);
-        if (file.exists()) {
-            Scanner s;
-            s = new Scanner(filename);
+        InputStream in = new FileInputStream(filename);
+        Reader reader = new InputStreamReader(in, Charset.defaultCharset());
+        int code;
+        while (((code = reader.read()) != -1) && x <= 9 && y <= 9) {
+            char symbol = (char) code;
+            System.out.println(symbol);
+            if (symbol == '\n') {
+                System.out.println("newline");
+                y = y + 1;
+                x = 0;
+            } else {
+                if (x < 9 && y < 9) {
+                    grid[x][y] = symbol;
+                    x = x + 1;
+                }
+            }
         }
     }
 }
