d49c17c3d7c010fc71f7d494e65d2bbd67b4c8d6
Louis Taylor <kragniz@gmail.com>
2013-10-13 23:54:46 +0100

    Add reading the .sud files into a 2d array

---
+++ src/eu/kragniz/sudoku/data/SextupleLinkedList.java
@@ -0,0 +1,8 @@
+package eu.kragniz.sudoku.data;
+
+/**
+ * Copyright 2013 Louis Taylor <kragniz@gmail.com>
+ * Licensed under GPLv3 or later
+ */
+public class SextupleLinkedList {
+}
--- src/eu/kragniz/sudoku/io/SudokuFile.java
+++ src/eu/kragniz/sudoku/io/SudokuFile.java
@@ -12,9 +12,11 @@ public class SudokuFile {
     private int[][] grid;
     private String filename;
 
+    private static final int SUDOKU_SIZE = 9;
+
     public SudokuFile(String filename) {
         this.filename = filename;
-        grid = new int[9][9];
+        grid = new int[SUDOKU_SIZE][SUDOKU_SIZE];
     }
 
     public int[][] getArray() {
@@ -28,19 +30,42 @@ public class SudokuFile {
         InputStream in = new FileInputStream(filename);
         Reader reader = new InputStreamReader(in, Charset.defaultCharset());
         int code;
+        //TODO listen to http://www.youtube.com/watch?v=-uCUgiop1GQ&list=PL1712EC828784E00F
         while (((code = reader.read()) != -1) && x <= 9 && y <= 9) {
             char symbol = (char) code;
             System.out.println(symbol);
             if (symbol == '\n') {
-                System.out.println("newline");
                 y = y + 1;
                 x = 0;
             } else {
                 if (x < 9 && y < 9) {
-                    grid[x][y] = symbol;
+                    if (symbol == ' ') {
+                        grid[x][y] = 0;
+                    } else {
+                        grid[x][y] = Integer.parseInt(symbol + ""); //TODO convert to int
+                    }
                     x = x + 1;
                 }
             }
         }
+    }
+
+    public String toString() {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < SUDOKU_SIZE; i++) {
+            for (int j = 0; j < SUDOKU_SIZE; j++) {
+                buf.append(grid[j][i]);
+                if (((j+1) % 3) == 0) {
+                    buf.append('|');
+                }
+            }
+
+            if (((i+1) % 3) == 0) {
+                buf.append("\n-----------\n");
+            } else {
+            buf.append('\n');
+            }
+        }
+        return buf.toString();
     }
 }
