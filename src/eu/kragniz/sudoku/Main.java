4274eae4e3f6961a25d38c4c9b1e99481a81c51e
Louis Taylor <kragniz@gmail.com>
2013-10-13 23:59:15 +0100

    Test the reading of .sud files

--- src/eu/kragniz/sudoku/Main.java
+++ src/eu/kragniz/sudoku/Main.java
@@ -2,6 +2,8 @@ package eu.kragniz.sudoku;
 
 import eu.kragniz.sudoku.io.SudokuFile;
 
+import java.io.IOException;
+
 public class Main {
 
     /* sextuple-linked list
@@ -16,5 +18,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello there");
         SudokuFile file = new SudokuFile("data/book55.sud");
+        try {
+            file.read();
+            System.out.print(file.toString());
+        } catch (IOException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
+        System.out.println("end of program");
     }
 }
