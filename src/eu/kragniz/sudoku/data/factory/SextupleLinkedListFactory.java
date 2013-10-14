b709de13518364e98ea6ed5cc7a0e5d0dd23597c
Louis Taylor <kragniz@gmail.com>
2013-10-14 15:17:50 +0100

    Add SextupleLinkedListFactory

---
+++ src/eu/kragniz/sudoku/data/factory/SextupleLinkedListFactory.java
@@ -0,0 +1,11 @@
+package eu.kragniz.sudoku.data.factory;
+
+/**
+ * Copyright 2013 Louis Taylor <kragniz@gmail.com>
+ * Licensed under GPLv3 or later
+ */
+public class SextupleLinkedListFactory {
+    public static getSextupleLinkedList(int[][] array) {
+
+    }
+}
