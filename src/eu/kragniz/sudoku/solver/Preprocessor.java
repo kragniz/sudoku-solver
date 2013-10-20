3e4fa01ec2c9731b2b890f0d9eb2a456afc3b735
Louis Taylor <kragniz@gmail.com>
2013-10-20 03:09:16 +0100

    Make Preprocessor neater and look at boxes

--- src/eu/kragniz/sudoku/solver/Preprocessor.java
+++ src/eu/kragniz/sudoku/solver/Preprocessor.java
@@ -3,8 +3,6 @@ package eu.kragniz.sudoku.solver;
 import eu.kragniz.sudoku.data.Cell;
 import eu.kragniz.sudoku.data.Sudoku;
 
-import java.util.HashSet;
-
 /**
  * Copyright 2013 Louis Taylor <kragniz@gmail.com>
  * Licensed under GPLv3 or later
@@ -22,17 +20,15 @@ public class Preprocessor extends Solver {
 
     public Sudoku solve() {
         for (int y = 0; y < 9; y++) {
-            HashSet<Integer> values = sudoku.getRowSet(y);
-            for (Cell cell : sudoku.getRow(y)) {
-                cell.removeAllPossibleValues(values);
-            }
+            Cell.removeValues(sudoku.getRow(y), sudoku.getRowSet(y));
         }
 
         for (int x = 0; x < 9; x++) {
-            HashSet<Integer> values = sudoku.getColumnSet(x);
-            for (Cell cell : sudoku.getColumn(x)) {
-                cell.removeAllPossibleValues(values);
-            }
+            Cell.removeValues(sudoku.getColumn(x), sudoku.getColumnSet(x));
+        }
+
+        for (int n = 0; n < 9; n++) {
+            Cell.removeValues(sudoku.getBox(n), sudoku.getBoxSet(n));
         }
         return sudoku;
     }
