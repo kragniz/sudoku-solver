f0896c73789859f6eada85ca049159cae913fb06
Louis Taylor <kragniz@gmail.com>
2013-10-20 03:13:11 +0100

    Added possible value Cell methods

--- src/eu/kragniz/sudoku/data/Cell.java
+++ src/eu/kragniz/sudoku/data/Cell.java
@@ -1,6 +1,5 @@
 package eu.kragniz.sudoku.data;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,7 +17,7 @@ public class Cell {
         digit = initialValue;
         possible = new HashSet<Integer>();
 
-        for (int i=1; i<=9; i++) {
+        for (int i = 1; i <= 9; i++) {
             possible.add(i);
         }
     }
@@ -27,23 +26,43 @@ public class Cell {
         this(0);
     }
 
-    public void setDigit(int digit) {
-        this.digit = digit;
-    }
-
     public int getDigit() {
         return digit;
     }
 
     public void removePossibleValue(int value) {
         possible.remove(value);
+        updateDigit();
+    }
+
+    public void removeAllPossibleValues(HashSet values) {
+        possible.removeAll(values);
+        updateDigit();
+    }
+
+    public void updateDigit() {
+        if (possible.size() == 1 && digit == 0) {
+            digit = possible.iterator().next();
+        }
     }
 
     public static HashSet cellArrayToIntSet(List<Cell> list) {
         HashSet<Integer> set = new HashSet<Integer>();
-        for (Cell cell: list) {
+        for (Cell cell : list) {
             set.add(cell.getDigit());
         }
         return set;
+    }
+
+    public static void removeValues(List<Cell> cells, HashSet<Integer> values) {
+        for (int y = 0; y < cells.size(); y++) {
+            for (Cell cell : cells) {
+                cell.removeAllPossibleValues(values);
+            }
+        }
+    }
+
+    public String toString() {
+        return String.valueOf(digit);
     }
 }
