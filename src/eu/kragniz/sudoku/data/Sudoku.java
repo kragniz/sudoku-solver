fa266d018a5ace415e0a05313ec897c0cb70bd4c
Louis Taylor <kragniz@gmail.com>
2013-10-20 03:07:46 +0100

    Add some box methods

--- src/eu/kragniz/sudoku/data/Sudoku.java
+++ src/eu/kragniz/sudoku/data/Sudoku.java
@@ -9,7 +9,6 @@ import java.util.List;
  * Licensed under GPLv3 or later
  */
 
-//TODO return a set for each row, column and section
 public class Sudoku {
     private Cell[][] grid;
 
@@ -33,13 +32,26 @@ public class Sudoku {
         return row;
     }
 
-
     public List<Cell> getColumn(int x) {
         List<Cell> column = new ArrayList<Cell>();
         for (int i = 0; i < 9; i++) {
             column.add(grid[x][i]);
         }
         return column;
+    }
+
+    public List<Cell> getBox(int x, int y) {
+        List<Cell> box = new ArrayList<Cell>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                box.add(grid[j + (x * 3)][i + (y * 3)]);
+            }
+        }
+        return box;
+    }
+
+    public List<Cell> getBox(int n) {
+        return getBox(n % 3, n / 3);
     }
 
     public HashSet getRowSet(int y) {
@@ -50,6 +62,14 @@ public class Sudoku {
         return Cell.cellArrayToIntSet(getColumn(x));
     }
 
+    public HashSet getBoxSet(int x, int y) {
+        return Cell.cellArrayToIntSet(getBox(x, y));
+    }
+
+    public HashSet getBoxSet(int n) {
+        return getBoxSet(n % 3, n / 3);
+    }
+
     public String toString() {
         StringBuffer buf = new StringBuffer();
         buf.append("\n-------------------------\n");
@@ -58,15 +78,15 @@ public class Sudoku {
             for (int j = 0; j < 9; j++) {
                 buf.append(grid[j][i].getDigit());
                 buf.append(" ");
-                if (((j+1) % 3) == 0) {
+                if (((j + 1) % 3) == 0) {
                     buf.append("| ");
                 }
             }
 
-            if (((i+1) % 3) == 0) {
+            if (((i + 1) % 3) == 0) {
                 buf.append("\n-------------------------\n");
             } else {
-            buf.append('\n');
+                buf.append('\n');
             }
         }
         return buf.toString();
