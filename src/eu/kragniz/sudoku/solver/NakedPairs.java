3b5336b272188494aac64bd5c8911c07e03c58a3
Louis Taylor <kragniz@gmail.com>
2013-10-24 20:23:19 +0100

    Added naked pairs implementation
    Worked first time!

--- src/eu/kragniz/sudoku/solver/NakedPairs.java
+++ src/eu/kragniz/sudoku/solver/NakedPairs.java
@@ -1,6 +1,9 @@
 package eu.kragniz.sudoku.solver;
 
+import eu.kragniz.sudoku.data.Cell;
 import eu.kragniz.sudoku.data.Sudoku;
+
+import java.util.List;
 
 /**
  * Copyright 2013 Louis Taylor <kragniz@gmail.com>
@@ -15,6 +18,22 @@ public class NakedPairs extends SolverStrategy {
 
     public void run() {
         setInactive();
+        for (List<Cell> group : sudoku.getGroups()) {
+            for (Cell cell : group) {
+                if (cell.getPossibleValues().size() == 2) {
+                    for (Cell searchCell : group) {
+                        if (searchCell.getPossibleValues().size() == 2 && cell != searchCell) {
+                            if (searchCell.getPossibleValues().equals(cell.getPossibleValues())) {
+                                for (Cell removeCell : group) {
+                                    if (removeCell != cell && removeCell != searchCell) {
+                                        addActive(removeCell.removeAllPossibleValues(cell.getPossibleValues()));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-
 }
